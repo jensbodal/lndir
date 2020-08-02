0207c238060141b47dee855c000448339c44e04b
Jens Bodal <jensbodal@gmail.com>
2020-08-02 10:00:53 -0700

    initial commit

---
+++ lndir.js
@@ -0,0 +1,118 @@
+#!/usr/bin/env node
+const os = require('os');
+const { existsSync, mkdirSync, renameSync, symlinkSync, writeFileSync }  = require('fs')
+const { resolve } = require('path');
+
+const readJson = filePath => JSON.parse(require(filePath));
+const writeJson = (filePath, json) => writeFileSync(filePath, JSON.stringify(json, null, 2));
+
+(async(argv) => {
+  const [ nodePath, scriptPath, ...args ] = argv;
+  const homeDir = os.homedir();
+  const configDir = `${homeDir}/.config/npm/lndir`;
+  const linksFilepath = `${configDir}/links.json`;
+
+  let linkThis;
+
+  // create config if it doesn't exist
+  if (!existsSync(linksFilepath)) {
+    mkdirSync(configDir, { recursive: true });
+    writeJson(linksFilepath, {});
+    console.log(`lndir map file created at: [${linksFilepath}]`);
+  };
+
+  console.log(`Links mapping file is located at [${linksFilepath}]`);
+  const mappings = require(linksFilepath);
+
+  if (args.length > 1) {
+    console.error('Either pass in the name of an existing link or the directory of one to create');
+    console.log(mappings);
+  }
+
+  if (args.length === 1) {
+    linkThis = args[0];
+  }
+
+  // No argument given, print mappings
+  if (!linkThis) {
+    console.log('You must pass a directory to this command to link or specify an existing mapping');
+    console.log(mappings);
+    return 0;
+  }
+
+  // check if we are given the name of a package to link and then try to link it
+  if (mappings[linkThis]) {
+    const mapping = mappings[linkThis];
+    const expectedLocalPackageDir = `./node_modules/${linkThis}`;
+
+    if (!existsSync(expectedLocalPackageDir)) {
+      console.error('You must install the package normally before replacing it with a linked version');
+      return 1;
+    }
+
+    const linkPathAt = `${expectedLocalPackageDir}/${mapping.linkedPath}`;
+    const backupPath = `${linkPathAt}.bak`;
+
+    console.log(`Linking [${mapping.absolutePath}] to [${linkPathAt}]`);
+
+    if (existsSync(backupPath)) {
+      console.error(`Backup exists at [${backupPath}]`);
+      console.error('Either restore this directory or remove it to continue');
+      return 1;
+    }
+
+    if (!existsSync(linkPathAt)) {
+      console.error(`Target path to replace with symlink does not exist [${linkPathAt}]`);
+      return 1;
+    }
+
+    if (existsSync(linkPathAt)) {
+      console.log(`Backing up [${linkPathAt}] to [${backupPath}]`);
+      renameSync(linkPathAt, backupPath);
+    }
+
+    symlinkSync(mapping.absolutePath, linkPathAt, 'dir');
+
+    return 0;
+  }
+
+  // assume we are trying to symlink a directory
+  if (linkThis) {
+    if (!existsSync(linkThis)) {
+      console.error(`Directory does not exist: ${linkThis}`);
+      return 1;
+    }
+
+    if (!existsSync('./package.json')) {
+      console.error('Script must be executed in the same directory that your package.json file exists at')
+      return 1;
+    }
+
+    const packageJson = require('./package.json');
+
+    if (!typeof packageJson === 'object' || !packageJson.name) {
+      console.error('package.json file must contain a name property');
+      return 1;
+    }
+
+    const linkName = packageJson.name;
+    const pathToLink = resolve(linkThis);
+
+    if (mappings[linkName]) {
+      console.log(`Mapping already exists for ${linkName} at ${mappings[linkName]}`);
+      console.log('Previous mapping will be removed');
+    }
+
+    console.log(`Creating link for ${linkName} at ${pathToLink}`)
+
+    mappings[linkName] = {};
+    mappings[linkName].absolutePath = pathToLink;
+    mappings[linkName].linkedPath = linkThis;
+
+    writeJson(linksFilepath, mappings);
+
+    console.log(mappings);
+
+    return 0;
+  }
+})(process.argv);
