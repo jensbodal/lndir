088d0f5cf7992632b6dd8befdfd77a2c96b020ab
Jens Bodal <jensbodal@gmail.com>
2020-08-02 10:04:12 -0700

    normalize return statements

--- lndir.js
+++ lndir.js
@@ -37,7 +37,7 @@ const writeJson = (filePath, json) => writeFileSync(filePath, JSON.stringify(jso
   if (!linkThis) {
     console.log('You must pass a directory to this command to link or specify an existing mapping');
     console.log(mappings);
-    return 0;
+    return;
   }
 
   // check if we are given the name of a package to link and then try to link it
@@ -47,7 +47,7 @@ const writeJson = (filePath, json) => writeFileSync(filePath, JSON.stringify(jso
 
     if (!existsSync(expectedLocalPackageDir)) {
       console.error('You must install the package normally before replacing it with a linked version');
-      return 1;
+      return;
     }
 
     const linkPathAt = `${expectedLocalPackageDir}/${mapping.linkedPath}`;
@@ -58,12 +58,12 @@ const writeJson = (filePath, json) => writeFileSync(filePath, JSON.stringify(jso
     if (existsSync(backupPath)) {
       console.error(`Backup exists at [${backupPath}]`);
       console.error('Either restore this directory or remove it to continue');
-      return 1;
+      return;
     }
 
     if (!existsSync(linkPathAt)) {
       console.error(`Target path to replace with symlink does not exist [${linkPathAt}]`);
-      return 1;
+      return;
     }
 
     if (existsSync(linkPathAt)) {
@@ -73,26 +73,26 @@ const writeJson = (filePath, json) => writeFileSync(filePath, JSON.stringify(jso
 
     symlinkSync(mapping.absolutePath, linkPathAt, 'dir');
 
-    return 0;
+    return;
   }
 
   // assume we are trying to symlink a directory
   if (linkThis) {
     if (!existsSync(linkThis)) {
       console.error(`Directory does not exist: ${linkThis}`);
-      return 1;
+      return;
     }
 
     if (!existsSync('./package.json')) {
       console.error('Script must be executed in the same directory that your package.json file exists at')
-      return 1;
+      return;
     }
 
     const packageJson = require('./package.json');
 
     if (!typeof packageJson === 'object' || !packageJson.name) {
       console.error('package.json file must contain a name property');
-      return 1;
+      return;
     }
 
     const linkName = packageJson.name;
@@ -113,6 +113,6 @@ const writeJson = (filePath, json) => writeFileSync(filePath, JSON.stringify(jso
 
     console.log(mappings);
 
-    return 0;
+    return;
   }
 })(process.argv);
